e0a53322dbaffb644d50b9dc7acef1aa9bbbf7d1
Tom Seifert <syex@seifertion.de>
2021-03-07 15:17:29 +0100

    Run Spotless

--- app/src/main/java/com/example/androiddevchallenge/TimerViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/TimerViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge
 
 import androidx.lifecycle.LiveData
@@ -5,7 +20,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -78,7 +92,7 @@ class TimerViewModel : ViewModel() {
     }
 
     fun timerChanged(value: String): String {
-        timerUserInput = if(value.length == 2 || value.length == 5) {
+        timerUserInput = if (value.length == 2 || value.length == 5) {
             "$value:"
         } else {
             value
